fa699b08de968c62bc22fc61c6bdb04fda23ba21
Tuan Nguyen <tuanna2704@gmail.com>
2017-09-27 02:57:01 -0500

    27/09

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get "post", to: "post#index", defaults: {format: :json}
   get "/", to: "frontend#index"
+  get '*path', to: 'frontend#index'
 end
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MainComponent }      from './components/main/main.component';
+import { BlogComponent }      from './components/blog/blog.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
+  { path: 'blog', component: BlogComponent },
   { path: '**', component: MainComponent }
 ];
 
@@ -14,4 +16,3 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule {}
-
